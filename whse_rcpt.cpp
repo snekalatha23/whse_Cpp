4407ba15fee37bad873b0496f2904f389877b5e3
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2020-02-12 13:53:41 +0530

    Update whse_rcpt.cpp

--- whse_rcpt.cpp
+++ whse_rcpt.cpp
@@ -1,92 +1,90 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <mysql.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib>
 
-/*
-typedef int MYSQL;
-extern MYSQL *mysql_init(MYSQL *mysql);
-extern MYSQL *mysql_real_connect(MYSQL *mysql, const char *host, const char *user, const char *passwd, const char *db, unsigned int port, const char *unix_socket, unsigned long client_flag);
-extern unsigned int mysql_errno(MYSQL *mysql);
-extern int mysql_query(MYSQL *mysql, const char *stmt_str);
-//*/
+using namespace std;
 
 struct Data
 {
-    char *i_num;
-    char *m_stat;
-    char *s_loc;
+    string i_num;
+    string m_stat;
+    string s_loc;
 };
 
-void getWareHouseAccess(char *DB_NAME,char *HOST, char *USER, char *PASS, char *TABLE_NAME, char *FILENAME)
-{
-    loggingFunction("getWareHouseAccess");
-    strcpy(DB_NAME, "warehouse_receipts");
-    strcpy(HOST, "WHSE_01");
-    strcpy(USER, "user");
-    strcpy(PASS, "accessSpecify");
-    strcpy(TABLE_NAME, "Received_Items");
-    strcpy(FILENAME, "N01.Z4");
-}
-
-int connect2Mysql(MYSQL **mysql, char *HOST, char *USER, char *PASS, char *DB_NAME, char *TABLE_NAME)
-{
-    loggingFunction("connect2Mysql");
-    *mysql = mysql_init(NULL);
-    mysql_real_connect(*mysql, HOST, USER, PASS, DB_NAME, 0, 0, 0);
-    return mysql_errno(*mysql);
-}
-
-void updateDatatoDB(MYSQL *mysql, FILE *myfile, struct Data *d, int i)
-{
-    loggingFunction("updateDatatoDB");
-    int j = 0;
-    while (j<i)
-    {
-        char * i_num = d[j].i_num;
-        char * stat = d[j].m_stat;
-        mysql_query(mysql, "UPDATE Received_Items  SET movement_status = stat WHERE item_nbr = i_num");
-        fprintf(myfile, "UPDATE Received_Items  SET movement_status = %s WHERE item_nbr = %s", stat, i_num);
-        j++;
-    }
-}
-
-int readDataFromFile(FILE *myfile, struct Data *d)
-{
-    loggingFunction("readDataFromFile");
-    int i=0;
-    size_t s;
-    while (myfile)
-    {
-        getline(&(d[i].i_num), &s, myfile);
-        getline(&(d[i].m_stat), &s, myfile);
-        getline(&(d[i++].s_loc), &s, myfile);
-    }
-
-    return i;
-}
-
 int main () {
+  
+                FILE *fp;
+                struct Data d[100];
+                char DB_NAME[1000],TABLE_NAME[1000],USER[1000],PASS[1000],FILEN[1000];
+                
+                strcpy(DB_NAME, getenv("DBNAME"));
+                strcpy(TABLE_NAME, getenv("TABLENAME"));
+                strcpy(USER, getenv("USERNAME"));
+                strcpy(PASS, getenv("PASSWORD"));
+                strcpy(HOST, getenv("HOST"));
+                strcpy(FILENAME, getenv("FILENAME"));
 
-    FILE *fp;
-    struct Data d[100];
-    char DB_NAME[1000], USER[1000], PASS[1000], TABLE_NAME[1000], FILENAME[1000];
-    int status;
+                fp = fopen(FILENAME, "w");
+                
+                int j = 0;
+                while (j<10)
+                {
+                                string i_num = d[j].i_num;
+                                string stat = d[j].m_stat;
+                                j++;
+                }
+                
+                MYSQL mysql;
+                status = connect2Mysql(mysql, HOST, USER, PASS, DB_NAME);
 
-    getWareHouseAccess(DB_NAME, HOST, USER, PASS, TABLE_NAME, FILENAME);
+                if(status== True)
+                {
+                                updateDB(mysql,fp,d);
+                                fprintf(fp, "UPDATE TABLE_NAME SET movement_status = %s WHERE item_nbr = %s", stat,i_num);
+                }
+                else
+                {
+                                updateFile(fp,d);
+                }              
 
-    fp = fopen(FILENAME, "w");
+                fclose(fp);
+                return 0;
 
-    int i = readDataFromFile(fp, d);
+}
 
-    MYSQL *mysql;
-    status = connect2Mysql(&mysql, HOST, USER, PASS, DB_NAME, TABLE_NAME);
+bool connect2Mysql(MYSQL *mysql, char *HOST, char *USER, char *PASS, char *DB_NAME)
+{
+                bool status;
+                mysql = mysql_init(NULL);
+                mysql_real_connect(mysql, HOST, USER, PASS, DB_NAME, 0, 0, 0);
+                return status;
+}
 
-    if(status)
-    {
-        updateDatatoDB(mysql, fp, d, i);
-    }
+void updateDB(MYSQL mysql,FILE *myfile,Data d)
+{
+                int j = 0;
+                while (j<10)
+                {
+                                string i_num = d[j].i_num;
+                                string stat = d[j].m_stat;
+                                mysql_query(mysql, "UPDATE TABLE_NAME SET movement_status = stat WHERE item_nbr = i_num");
+                                j++;
+                }
+                updateFile(myfile,d);
+                
+}
 
-    fclose(fp);
-    return 0;
+void updateFile(FILE *myfile,Data d)
+{
+                int i=0;
+                
+                while (myfile)
+                {
+                                getline(myfile, d[i].i_num, ',');
+                                getline(myfile, d[i].m_stat, ',');
+                                getline(myfile, d[i++].s_loc, '\n');
+                }
 }
